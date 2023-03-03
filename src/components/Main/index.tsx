8d61e2a11cde97192269dbb168e571667de0598f
Larissa Hora <37601767+larissahora@users.noreply.github.com>
2023-03-02 22:40:23 -0300

    Initial commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Icon de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Uma pessoa sentada em frente ao computador"
+    />
+  </S.Wrapper>
+)
+
+export default Main
